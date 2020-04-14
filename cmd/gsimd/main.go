f10f5d9a947c93c29b59e28a84dd1116f8ef894e
imlonghao <git@imlonghao.com>
2020-04-14 23:58:26 +0800

    Add results cleaner

--- cmd/gsimd/main.go
+++ cmd/gsimd/main.go
@@ -12,6 +12,30 @@ import (
 
 var taskIsRunning map[uint]bool
 
+func isMatchWhitelist(result types.Result, whitelists []types.Whitelist) bool {
+	for _, whitelist := range whitelists {
+		switch whitelist.Type {
+		case 1:
+			if result.Username == whitelist.Content {
+				return true
+			}
+		case 2:
+			if strings.Contains(result.Repo, whitelist.Content) {
+				return true
+			}
+		case 3:
+			if strings.Contains(result.Code, whitelist.Content) {
+				return true
+			}
+		case 4:
+			if strings.Contains(result.Path, whitelist.Content) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func worker(task types.Task) {
 	defer func() {
 		fmt.Printf("Task %d is finishing\n", task.ID)
@@ -26,31 +50,7 @@ func worker(task types.Task) {
 	}
 	whitelists := db.GetWhitelistsForID(task.ID)
 	for _, result := range results {
-		matchWhitelist := false
-		for _, whitelist := range whitelists {
-			switch whitelist.Type {
-			case 1:
-				if result.Username == whitelist.Content {
-					matchWhitelist = true
-					break
-				}
-			case 2:
-				if strings.Contains(result.Repo, whitelist.Content) {
-					matchWhitelist = true
-					break
-				}
-			case 3:
-				if strings.Contains(result.Code, whitelist.Content) {
-					matchWhitelist = true
-					break
-				}
-			case 4:
-				if strings.Contains(result.Path, whitelist.Content) {
-					matchWhitelist = true
-					break
-				}
-			}
-		}
+		matchWhitelist := isMatchWhitelist(result, whitelists)
 		existed := db.IfResultExisted(result.ID)
 		if matchWhitelist {
 			if existed {
@@ -68,6 +68,16 @@ func worker(task types.Task) {
 	}
 }
 
+func whitelistFilter(id uint, whitelists []types.Whitelist) []types.Whitelist {
+	var result []types.Whitelist
+	for _, whitelist := range whitelists {
+		if whitelist.TaskID == id {
+			result = append(result, whitelist)
+		}
+	}
+	return result
+}
+
 func main() {
 	taskIsRunning = make(map[uint]bool)
 	c := cron.New()
@@ -82,6 +92,18 @@ func main() {
 			fmt.Printf("Task %d is starting\n", task.ID)
 			taskIsRunning[task.ID] = true
 			go worker(task)
+		}
+	})
+	c.AddFunc("*/30 * * * *", func() {
+		fmt.Printf("Running cron job results cleaner\n")
+		var results []types.Result
+		var whitelists []types.Whitelist
+		db.DB.Table("results").Find(&results)
+		db.DB.Table("whitelists").Find(&whitelists)
+		for _, result := range results {
+			if isMatchWhitelist(result, whitelistFilter(result.TaskID, whitelists)) {
+				db.DB.Table("results").Delete(&result)
+			}
 		}
 	})
 	c.Start()
